c6416e405efa6f856068b3eb1ca5ec622b8d4525
4nc3str4l <cmuri_10@hotmail.com>
2021-03-30 22:09:55 +0200

    Setting up a golang websocket server

---
+++ src/websocket-backend/src/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+const address = "0.0.0.0:27017"
+
+var addr = flag.String("addr", address, "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func GeneralStats(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/", GeneralStats)
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	log.Printf("Websocket Server running in %s\n", address)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
