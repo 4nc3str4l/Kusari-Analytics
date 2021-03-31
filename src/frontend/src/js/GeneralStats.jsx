b9febd23d4a940c940ab43ae119904b882673512
4nc3str4l <cmuri_10@hotmail.com>
2021-04-01 01:06:25 +0200

    Now the frontend shows the fake block number in real time

---
+++ src/frontend/src/js/GeneralStats.jsx
@@ -0,0 +1,43 @@
+"use strict";
+
+import ReconnectingWebsocket from 'reconnecting-websocket';
+
+import React, {Component, Fragment} from "react";
+
+import {EP_GENERAL_STATS} from "./constants";
+
+class GeneralStats extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            currentBlock: 0
+        }
+        this.ws = null;
+    }
+
+    componentWillMount() {
+        this.ws = new ReconnectingWebsocket(EP_GENERAL_STATS);
+        this.ws.addEventListener('message', this.onDataUpdate.bind(this));
+    }
+
+    onDataUpdate(message){
+        let packet = JSON.parse(message.data);
+        this.setState({
+            ...this.state,
+            currentBlock: packet.block_num
+        })
+    }
+
+    componentWillUnmount() {
+        this.ws.removeEventListener('message', this.onDataUpdate.bind(this));
+        this.ws.close();
+    }
+
+    render(){
+        return(<p>Current block {this.state.currentBlock}</p>);
+    }
+}
+
+export default GeneralStats;
